44f35a1e651ea2b31001e899b73c6560e4552018
jdrhea <93232701+jdrhea@users.noreply.github.com>
2022-12-04 18:49:09 -0600

    add swipe functionality

--- app.js
+++ app.js
@@ -160,6 +160,34 @@ document.addEventListener('DOMContentLoaded', () =>  {
     }
   }
   document.addEventListener('keyup', control)
+  document.addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+  }, false);
+
+  document.addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+  }, false);
+
+  function handleGesture() {
+    if (touchendX < touchstartX) {
+      keyLeft()
+    }
+
+    if (touchendX > touchstartX) {
+      keyRight()
+    }
+
+    if (touchendY < touchstartY) {
+      keyUp()
+    }
+
+    if (touchendY > touchstartY) {
+      keyDown()
+    }
+  }
 
   function keyRight() {
     moveRight()
